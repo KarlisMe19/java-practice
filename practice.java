f6e973b0b38ddd074da8347651ebbebb2cf27eb5
Karl <laurente.karlandreigarcia10II@gmail.com>
2026-02-22 23:22:39 +0800

    test

--- practice.java
+++ practice.java
@@ -1,25 +1,6 @@
-public class practice {
-
+public class test {
+    
     public static void main(String[] args){
-        String firstName = "John ";
-        String lastName = "Doe";
-        String fullName = firstName + lastName;
-        System.out.println(fullName);
-        int num1 = 6, num2 = 7;
-        System.out.println("The sum is " + num1 + num2);   // Prints: The sum is 56
-        System.out.println("The sum is " + (num1 + num2)); // Prints: The sum is 11
-
-        float decimalNum = 9.789f;
-        long longNum = 993928390582893L;
-        double doubleNum = 87.9023d;
-        System.out.println(decimalNum);
-        System.out.println(longNum);
-        System.out.println(doubleNum);
-// Beginner
-// Print numbers 1–100, but replace multiples of 3 with "Fizz", multiples of 5 with "Buzz", and multiples of both with "FizzBuzz"
-// Check if a number is even or odd
-// Find the largest of three numbers
-// Calculate the factorial of a number
-// Reverse a string
+        
     }
 }
