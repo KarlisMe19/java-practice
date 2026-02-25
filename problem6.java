4323e134af36d30ec1619b9a0ee57f9155236668
Karl <laurente.karlandreigarcia10II@gmail.com>
2026-02-25 23:34:28 +0800

    "problem 6"

---
+++ problem6.java
@@ -0,0 +1,17 @@
+public class problem6 {
+
+    public problem6 (int ID, String name, int grade){
+
+    }
+
+    public static void main(String[] args){
+// Create a program that:
+// Stores students (ID, name, list of grades)
+// Computes:
+// Average grade per student
+// Highest grade
+// Overall class average
+// Allows searching student by ID
+// Displays students sorted by average (highest to lowest)
+    }
+}
