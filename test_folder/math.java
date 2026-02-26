c53c5d5c683099536c89939c0b0934b4cdb9bff6
Karl <laurente.karlandreigarcia10II@gmail.com>
2026-02-26 20:10:36 +0800

    added math.java

---
+++ test_folder/math.java
@@ -0,0 +1,10 @@
+package test_folder;
+
+public class math {
+    
+    public static void main(String[] args) {
+    int[] numbers = {1, 2, 3, 4, 5};
+    int max_number = Math.max(2,3);
+    System.out.println("The max number is: " + max_number);
+    }
+}
