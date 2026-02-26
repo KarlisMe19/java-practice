1a5e25f9758ded85ed5dda2a1a2b8d541be3cc2d
Karl <laurente.karlandreigarcia10II@gmail.com>
2026-02-26 23:04:24 +0800

    added a new file

--- problem8.java
+++ problem8.java
@@ -1,9 +1,18 @@
+import java.util.Scanner;
 import java.text.NumberFormat;
 
 public class problem8 {
     public static void main(String[] args) {
-    NumberFormat percent = NumberFormat.getPercentInstance();
-    String result = percent.format(.1);
-    System.out.println(result);
+    System.out.print("Please enter a number: ");
+    Scanner scanner = new Scanner(System.in);
+    String textSample = scanner.nextLine();
+
+    System.out.println("Your typed text is: " + textSample.trim());
+
+    // float percentNumber = scanner.nextFloat();
+    // String result = NumberFormat.getPercentInstance().format(percentNumber/100);
+    // System.out.println("Your number in percentage is: " + result);
+
+    scanner.close();
     }
 }
---
+++ studentProgram.java
@@ -0,0 +1,25 @@
+public class studentProgram {
+    public static void main(String[] args) {
+    // Create a program that:
+    // Stores students (ID, name, list of grades)
+    // Computes:
+    // Average grade per student
+    // Highest grade
+    // Overall class average
+    // Allows searching student by ID
+    // Displays students sorted by average (highest to lowest)
+    int money = 200_000;
+    String financialStatus = (money >= 200_000) ? "Very very rich" : "Very very poor";
+
+    System.out.println(financialStatus);
+    }
+
+    // int ID;
+    // String name;
+    // int[] grades;
+    // public void studentInfo(int ID, String name, int[] grades) {
+        // this.ID = ID;
+        // this.name = name;
+        // this.grades = grades;
+    // }
+}
